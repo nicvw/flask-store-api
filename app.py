fc454d2a7e389b5afb8385ba022704d63c7888d1
Nicholas von Waltsleben <waltsleben@gmail.com>
2021-10-30 13:00:41 +0200

    enable database connection string via env

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request
 from flask_jwt import JWT, jwt_required
 from flask_restful import Api, Resource, reqparse
@@ -9,9 +11,9 @@ from resources.user import UserRegister
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
-app.secret_key = '123qwe'
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///data.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+app.secret_key = "123qwe"
 
 
 @app.before_first_request
@@ -22,13 +24,13 @@ def create_tables():
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api = Api(app=app)
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(StoreList, '/stores')
-api.add_resource(UserRegister, '/register')
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+api.add_resource(StoreList, "/stores")
+api.add_resource(UserRegister, "/register")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     db.init_app(app)
     app.run(port=5000, debug=True)
