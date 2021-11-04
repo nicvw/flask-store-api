10fbddbb0f6fabe297ada8e16666d5cb02beb46c
Nicholas von Waltsleben <waltsleben@gmail.com>
2021-11-04 07:57:35 +0200

    update length of password column to support hashes

--- models/user.py
+++ models/user.py
@@ -10,7 +10,7 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(80))
+    password = db.Column(db.String(102))
 
     def __init__(self, username: str, password: str):
         self.username = username
